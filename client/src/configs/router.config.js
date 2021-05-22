2ff3292130b2252c8006c3ed713210b82a42398d
Jay <nhanlnt@hotmail.com>
2021-05-22 22:29:06 +0700

    Client - Delete component and container folder

--- client/src/configs/router.config.js
+++ client/src/configs/router.config.js
@@ -1,27 +1,8 @@
-import PostBorder from '../components/postBorder';
-import PageNotFound from '../containers/pageNotFound';
-import HomePage from '../containers/homepage';
-import CreatePost from '../components/createPost';
-
-export const routes = [
-  {
-    path: '/',
-    isExact: true,
-    module: HomePage
-  },
-  {
-    path: '/create-post',
-    isExact: true,
-    module: CreatePost //just for design will delete when project done
-  },
-  {
-    path: '/post',
-    isExact: true,
-    module: PostBorder //just for design will delete when project done
-  },
-  {
-    path: '*',
-    isExact: true,
-    module: PageNotFound
-  }
-]
+// import PostBorder from '../components/postBorder';
+// import PageNotFound from '../containers/pageNotFound';
+// import HomePage from '../containers/homepage';
+// import CreatePost from '../components/createPost';
+//
+// export const routes = [
+//
+// ]
