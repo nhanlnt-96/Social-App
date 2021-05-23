09aa1ab3cfd3f4562e033eaba367916e63bb4b08
Jay <nhanlnt@hotmail.com>
2021-05-23 21:56:41 +0700

    Client - scss folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,34 @@
-import './scss/App.scss';
+import React from 'react';
 import RouterOutlet from './routes/RouterOutlet';
 
+//import component
+import { Navigation } from './components';
+
+//import styles
+import './scss/App.scss';
 import 'antd/dist/antd.css';
+
+//import layout antd
+import { Layout } from 'antd';
+
+const {Header, Footer, Sider, Content} = Layout;
 
 function App() {
   return (
-    <RouterOutlet/>
+    <div className="App">
+      <Layout>
+        <Header className="header">
+          <Navigation/>
+        </Header>
+        <Layout>
+          <Sider>Sider</Sider>
+          <Content>
+            content
+          </Content>
+        </Layout>
+        <Footer>Footer</Footer>
+      </Layout>
+    </div>
   );
 }
 
