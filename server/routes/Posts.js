961ae613132d0816fc584c6065c5c436a9b3e098
Jay <nhanlnt@hotmail.com>
2021-05-29 10:11:38 +0700

    Server - Complete create route get post by id

--- server/routes/Posts.js
+++ server/routes/Posts.js
@@ -15,4 +15,12 @@ router.get('/', async (req, res) => {
   res.json(allPosts);
 });
 
+//individual pages base on ID
+router.get('/post-by-id/:id', async (req, res) => {
+  const id = req.params.id; 
+  const postById = await Posts.findByPk(id);
+  res.json(postById);
+})
+
+
 module.exports = router;
