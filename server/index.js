036f2531b04f26366eb6acb1815e1be69f482212
Jay <nhanlnt@hotmail.com>
2021-05-31 20:44:15 +0700

    Server - Complete create register and login route

--- server/index.js
+++ server/index.js
@@ -10,8 +10,12 @@ const db = require('./models');
 //Routers
 const postRouter = require('./routes/Posts');
 app.use('/posts', postRouter);
+
 const commentRouter = require('./routes/Comments');
 app.use('/comments', commentRouter);
+
+const userRouter = require('./routes/Users');
+app.use('/auth', userRouter);
 
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
