d3143c7680dac558a64f7d6b704730c2745b31c9
Jay <nhanlnt@hotmail.com>
2021-06-02 22:23:21 +0700

    Client - Working on JWT auth

--- client/src/network/services/auth.js
+++ client/src/network/services/auth.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 const baseURL = 'http://localhost:3001/auth';
 
@@ -13,6 +14,9 @@ export const loginRequest = async (values) => {
   return axios.post(`${baseURL}/login`, {
     username: values.username,
     password: values.password
+  }).then((response) => {
+    const accessToken = response.data;
+    Cookies.set('access-token', accessToken);
   })
 }
 
