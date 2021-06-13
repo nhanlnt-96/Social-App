0e425ba3f6e34d26637b776b440586836ae29e49
Jay <nhanlnt@hotmail.com>
2021-06-13 20:12:47 +0700

    Prepare to publish

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
 
 app.use(cors());
 app.use(express.json());
