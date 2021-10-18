57f38f923c191bea26558b44ece788ab2c363678
Jay <nhanlnt@hotmail.com>
2021-10-18 21:53:08 +0700

    Deploy

--- server_mongodb/index.js
+++ server_mongodb/index.js
@@ -9,16 +9,18 @@ View.settings.set("views", "./configs");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const corsOptions = {
-  origin: "http://localhost:3000",
-  optionSuccessStatus: 200,
-};
+// use when run at local
+// const corsOptions = {
+//   origin: "http://localhost:3000",
+//   optionSuccessStatus: 200,
+// };
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json());
-app.use(cors(corsOptions));
-// app.use(cors());
+// use when run at local
+// app.use(cors(corsOptions));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello, welcome to Social-App-Api");
